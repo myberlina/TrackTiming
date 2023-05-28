223feebfffaaa1f8dcf4624441783ee9de1f2821
David Deaves <HSV_Timing@dd.id.au>
2023-05-28 22:14:32 +1000

    Modify queries to allow reporting red lights
    VIEW results
    * Join to start_time allows green_time.time_ms-2000 < start_time.time_ms
    * New field 'red' is boolean (1) if run red lighted
    
    VIEW rt_order, et_order, ft_order
    * pass through 'red' field from results
    * when ordering now use {r,e,f}t_ms + red*10000000 to ensure that any
      non red run sorts before all red runs, and if all runs are red, you
      are then still included, and sorted based on best red time

--- Create_Database.sql
+++ Create_Database.sql
@@ -36,11 +36,12 @@ CREATE TABLE IF NOT EXISTS finish_time ( event INT, run INT, car_num INT, time_m
 
 
 DROP VIEW results;
-CREATE VIEW results (event , run , car_num , rt_ms , et_ms , ft_ms )
+CREATE VIEW results (event , run , car_num , rt_ms , et_ms , ft_ms , red )
 as select green_time.event, green_time.run, green_time.car_num,
        (start_time.time_ms - green_time.time_ms),
        (finish_time.time_ms - start_time.time_ms),
-       (finish_time.time_ms - green_time.time_ms)
+       (finish_time.time_ms - min(green_time.time_ms, start_time.time_ms)),
+       (start_time.time_ms < green_time.time_ms)
 from green_time
  left join finish_time on green_time.event = finish_time.event
                      and green_time.run = finish_time.run
@@ -49,19 +50,25 @@ from green_time
  left join start_time on green_time.event = start_time.event
                      and green_time.run = start_time.run
                      and green_time.car_num = start_time.car_num
-                     and green_time.time_ms < start_time.time_ms
+                     and green_time.time_ms-2000 < start_time.time_ms
 where start_time.time_ms < finish_time.time_ms
-/* results(event,run,car_num,rt_ms,et_ms,ft_ms) */;
+/* results(event,run,car_num,rt_ms,et_ms,ft_ms,red) */;
 
-CREATE VIEW rt_order (event , run , car_num, best_rt)
-as select event, run, car_num, min(rt_ms) from results group by event, car_num order by min(rt_ms)
-/* rt_order(event,run,car_num,best_rt) */;
 
-CREATE VIEW et_order (event , run , car_num, best_et)
-as select event, run, car_num, min(et_ms) from results group by event, car_num order by min(et_ms)
-/* et_order(event,run,car_num,best_et) */;
+DROP VIEW rt_order;
+CREATE VIEW rt_order (event , run , car_num, best_rt, red)
+as select event, run, car_num, rt_ms, red from results group by event, car_num order by red, min(red*10000000+rt_ms)
+/* rt_order(event,run,car_num,best_rt,red) */;
 
-CREATE VIEW ft_order (event , run , car_num, best_ft)
-as select event, run, car_num, min(ft_ms) from results group by event, car_num order by min(ft_ms)
-/* ft_order(event,run,car_num,best_ft) */;
+DROP VIEW et_order;
+CREATE VIEW et_order (event , run , car_num, best_et, red)
+as select event, run, car_num, et_ms, red from results group by event, car_num order by red, min(red*10000000+et_ms)
+/* et_order(event,run,car_num,best_et,red) */;
+
+DROP VIEW ft_order;
+CREATE VIEW ft_order (event , run , car_num, best_ft, red)
+as select event, run, car_num, ft_ms, red from results group by event, car_num order by red, min(red*10000000+ft_ms)
+/* ft_order(event,run,car_num,best_ft,red) */;
+
+COMMIT;
 
