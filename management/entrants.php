cd3a9b69edae31fd60f213e46c2108393b4c196f
David Deaves <HSV_Timing@dd.id.au>
2023-06-05 11:26:35 +1000

    Added a speical filed to entrant entry.
    Added a speical filed to entrant entry.
    Allow to specify Lady, or Junior.... to report
    a separate place within that catagory

--- management/entrants.php
+++ management/entrants.php
@@ -14,11 +14,12 @@
   if(($evt>0)&&(count($_POST)>0)) {
     include_once 'database.php';
     if(isset($_POST['submit'])&&('Update' == $_POST['submit'])&&($row_id>0)) {
-      if ($post_qry = $db->prepare("UPDATE entrant_info set car_num=:num, car_name=:name, car_info=:info WHERE rowid=:row AND event=:event")){
+      if ($post_qry = $db->prepare("UPDATE entrant_info set car_num=:num, car_name=:name, car_info=:info, special=:special WHERE rowid=:row AND event=:event")){
         $post_qry->bindValue(':event', 0 + $db->escapeString($evt), SQLITE3_INTEGER);
         $post_qry->bindValue(':num', 0 + $db->escapeString($_POST["EntNum-$row_id"]), SQLITE3_INTEGER);
         $post_qry->bindValue(':name', $db->escapeString($_POST["EntName-$row_id"]), SQLITE3_TEXT);
         $post_qry->bindValue(':info', $db->escapeString($_POST["EntInfo-$row_id"]), SQLITE3_TEXT);
+        $post_qry->bindValue(':special', $db->escapeString($_POST["EntSpecial-$row_id"]), SQLITE3_TEXT);
         $post_qry->bindValue(':row', 0 + $row_id, SQLITE3_INTEGER);
         if ($update_result = $post_qry->execute())
           $message = "<font color=\"#00a000\"> Record Modified Successfully";
@@ -31,11 +32,12 @@
     }
 
     if(isset($_POST['submit']) && ('Create' == $_POST['submit'])) {
-      if ($post_qry = $db->prepare("INSERT INTO entrant_info(event, car_num, car_name, car_info) VALUES(:event, :num, :name, :info)")){
+      if ($post_qry = $db->prepare("INSERT INTO entrant_info(event, car_num, car_name, car_info, special) VALUES(:event, :num, :name, :info, :special)")){
         $post_qry->bindValue(':event', 0 + $db->escapeString($evt), SQLITE3_INTEGER);
         $post_qry->bindValue(':num', 0 + $db->escapeString($_POST["EntNum-$row_id"]), SQLITE3_INTEGER);
         $post_qry->bindValue(':name', $db->escapeString($_POST["EntName-$row_id"]), SQLITE3_TEXT);
         $post_qry->bindValue(':info', $db->escapeString($_POST["EntInfo-$row_id"]), SQLITE3_TEXT);
+        $post_qry->bindValue(':special', $db->escapeString($_POST["EntSpecial-$row_id"]), SQLITE3_TEXT);
         if ($update_result = $post_qry->execute()) 
           $message = "<font color=\"#00a000\"> Record Created Successfully";
         else
@@ -46,11 +48,12 @@
         $message = "<font color=\"#c00000\"> Record Create failed for &nbsp; ".$_POST["EntNum-$row_id"].", \"".$_POST["EntName-$row_id"]."\"\n<BR>". $db->lastErrorMsg();
     }
     if(isset($_POST['really-delete'])&&('Yes!' == $_POST['really-delete'])&&($row_id>0)) {
-      if ($post_qry = $db->prepare("DELETE FROM entrant_info WHERE event=:event AND car_num=:num AND car_name=:name AND car_info=:info AND rowid=:row")){
+      if ($post_qry = $db->prepare("DELETE FROM entrant_info WHERE event=:event AND car_num=:num AND car_name=:name AND car_info=:info AND special=:special AND rowid=:row")){
         $post_qry->bindValue(':event', 0 + $db->escapeString($evt), SQLITE3_INTEGER);
         $post_qry->bindValue(':num', 0 + $db->escapeString($_POST["EntNum-$row_id"]), SQLITE3_INTEGER);
         $post_qry->bindValue(':name', $db->escapeString($_POST["EntName-$row_id"]), SQLITE3_TEXT);
         $post_qry->bindValue(':info', $db->escapeString($_POST["EntInfo-$row_id"]), SQLITE3_TEXT);
+        $post_qry->bindValue(':special', $db->escapeString($_POST["EntSpecial-$row_id"]), SQLITE3_TEXT);
         $post_qry->bindValue(':row', 0 + $row_id, SQLITE3_INTEGER);
         if ($update_result = $post_qry->execute())
           $message = "<font color=\"#00a000\"> Record Deleted Successfully for ".$_POST["EntNum-$row_id"].", \"".$_POST["EntName-$row_id"]."\"\n<BR>";
@@ -104,7 +107,7 @@
     $cur_run = $row["current_run"];
   }
 
-  if ($ent_qry = $db->prepare('SELECT rowid, car_num, car_name, car_info FROM entrant_info WHERE event = :event ORDER BY car_num')) {
+  if ($ent_qry = $db->prepare('SELECT rowid, car_num, car_name, car_info, special FROM entrant_info WHERE event = :event ORDER BY car_num')) {
     $ent_qry->bindValue(':event', 0 + $evt, SQLITE3_INTEGER);
     $entrants = $ent_qry->execute();
   }
@@ -137,6 +140,7 @@
       <td width=50>Num</td>
       <td>Title</td>
       <td>Info/Model</td>
+      <td>Special</td>
       <td> <input id="delete-ALL" type="button" name="delete-ALL" value="Delete ALL" onclick="document.getElementById('really-ALL').disabled=false" class="button"> </td>
       <td> <input id="really-ALL" type="button" name="really-ALL" value="Really?" onclick="document.getElementById('really-delete-ALL').disabled=false" class="button" disabled> </td>
       <td> <input id="really-delete-ALL" type="submit" name="really-delete-ALL" value="Yes!" formnovalidate formaction="?evt=<?php echo $evt;?>&id=ALL" class="button" disabled> </td>
@@ -153,6 +157,7 @@
     $safe_num=htmlspecialchars($row['car_num']);
     $safe_name=htmlspecialchars($row['car_name']);
     $safe_info=htmlspecialchars($row['car_info']);
+    $safe_special=htmlspecialchars($row['special']);
     $row_id=$row['rowid'];
     echo "<td><input type=\"number\" placeholder=\"Num\" size=\"4\" name=\"EntNum-$row_id\" required min=\"1\" value=\"$safe_num\"";
     echo " class=\"input_number\" oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_num')\" ></td>\n";
@@ -160,6 +165,8 @@
     echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_name')\" ></td>\n";
     echo "<td><input type=\"text\" placeholder=\"Entrant Info\" name=\"EntInfo-$row_id\" class=\"txtField\" value=\"$safe_info\"";
     echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_info')\" ></td>\n";
+    echo "<td><input type=\"text\" placeholder=\"Entrant Special\" name=\"EntSpecial-$row_id\" class=\"txtField\" value=\"$safe_special\"";
+    echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_special')\" ></td>\n";
     echo "<td> <input id=\"submit-$row_id\" type=\"submit\" name=\"submit\" value=\"Update\" formaction=\"?evt=$evt&id=$row_id\" class=\"button\" disabled> </td>\n";
     echo "<td> <input id=\"delete-$row_id\" type=\"button\" name=\"delete-$row_id\" value=\"Delete\" onclick=\"document.getElementById('really-delete-$row_id').disabled=false\" class=\"button\"> </td>\n";
     echo "<td> <input id=\"really-delete-$row_id\" type=\"submit\" name=\"really-delete\" value=\"Yes!\" formnovalidate formaction=\"?evt=$evt&id=$row_id\" class=\"button\" disabled> </td>\n";
@@ -176,13 +183,15 @@
    else
     $classname="class=\"oddRow\"";
    echo "<tr $classname>";
-   $safe_num=""; $safe_name=""; $safe_info=""; $row_id=0;
+   $safe_num=""; $safe_name=""; $safe_info=""; $safe_special=""; $row_id=0;
    echo "<td><input type=\"number\" placeholder=\"Num\" size=\"4\" name=\"EntNum-$row_id\" required min=\"$min_evt\" value=\"$min_evt\"";
    echo " class=\"input_number\" ></td>\n";
    echo "<td><input type=\"text\" placeholder=\"Entrant Name\" name=\"EntName-$row_id\" class=\"txtField\" value=\"$safe_name\"";
    echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_name')\" ></td>\n";
    echo "<td><input type=\"text\" placeholder=\"Entrant Info\" name=\"EntInfo-$row_id\" class=\"txtField\" value=\"$safe_info\"";
    echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_info')\" ></td>\n";
+   echo "<td><input type=\"text\" placeholder=\"Entrant Special\" name=\"EntSpecial-$row_id\" class=\"txtField\" value=\"$safe_special\"";
+   echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_special')\" ></td>\n";
    echo "<td> <input id=\"submit-$row_id\" type=\"submit\" name=\"submit\" value=\"Create\" formaction=\"?evt=$evt&id=$row_id\" class=\"button\" disabled> </td>\n";
    echo "</td></tr>\n";
    ?>
