e243826c4e0b137556daf72667d7ce404b6d5f93
David Deaves <HSV_Timing@dd.id.au>
2023-06-07 17:30:30 +1000

    Update All
    Added an Update All to the page so that multiple updates
    can be made in one submit.
    
    Useful for marking all the Ladys in one go
    
    Also fixed the width of the Special field.

--- management/entrants.php
+++ management/entrants.php
@@ -64,6 +64,38 @@
       else
         $message = "<font color=\"#c00000\"> Record Delete failed for &nbsp; ".$_POST["EntNum-$row_id"].", \"".$_POST["EntName-$row_id"]."\"\n<BR>". $db->lastErrorMsg();
     }
+    if(isset($_POST['really-delete-ALL'])&&('Yes' == $_POST['really-delete-ALL'])&&
+             isset($_POST['update_list'])&&("" != $_POST['update_list'])) {
+      //var_dump($_POST);
+      $update_list = explode (";submit-", $_POST['update_list']); 
+      //var_dump($update_list);
+      if (sizeof($update_list) > 1) {
+        if ($post_qry = $db->prepare("UPDATE entrant_info set car_num=:num, car_name=:name, car_info=:info, special=:special WHERE rowid=:row AND event=:event")){
+	  $good=0;
+	  $bad_message="";
+          foreach($update_list as $row_id) {
+	    if ($row_id < 1) continue;
+            $post_qry->bindValue(':event', 0 + $db->escapeString($evt), SQLITE3_INTEGER);
+            $post_qry->bindValue(':num', 0 + $db->escapeString($_POST["EntNum-$row_id"]), SQLITE3_INTEGER);
+            $post_qry->bindValue(':name', $db->escapeString($_POST["EntName-$row_id"]), SQLITE3_TEXT);
+            $post_qry->bindValue(':info', $db->escapeString($_POST["EntInfo-$row_id"]), SQLITE3_TEXT);
+            $post_qry->bindValue(':special', $db->escapeString($_POST["EntSpecial-$row_id"]), SQLITE3_TEXT);
+            $post_qry->bindValue(':row', 0 + $row_id, SQLITE3_INTEGER);
+            if ($update_result = $post_qry->execute())
+              $good++;
+            else
+              $bad_message = $bad_message." ".$_POST["EntNum-$row_id"].", \"".$_POST["EntName-$row_id"]."\"\n<BR>". $db->lastErrorMsg();
+          }
+	  if ($good > 0)
+	    $message = "<font color=\"#00a000\"> $good records modified successfully";
+	  if ($bad_message != "")
+	    $message = $message."<BR><font color=\"#c00000\"> Record Modify failed for $bad_message";
+          $post_qry->close();
+        }
+        else
+          $message = "<font color=\"#c00000\"> Record Modify failed for all\n<BR>". $db->lastErrorMsg();
+      }
+    }
     if(isset($_POST['really-delete-ALL'])&&('Yes!' == $_POST['really-delete-ALL'])) {
       if ($post_qry = $db->prepare("DELETE FROM entrant_info WHERE event=:event")){
         $post_qry->bindValue(':event', 0 + $db->escapeString($evt), SQLITE3_INTEGER);
@@ -125,7 +157,31 @@
 <center>
 <h2>Entrant Management</h2>
   <form name="frmEntrant" method="post" action="">
-<script type="text/javascript">function showEntrants(str){document.location = 'entrants.php?evt='+str;}</script>
+    <input type="hidden" name="update_list" value="" id="update_list">
+  <script type="text/javascript">
+    function showEntrants(str){document.location = 'entrants.php?evt='+str;};
+    function haveUpdate(){
+            update_list="";
+	    update_count=0;
+	    up_buttons=document.getElementsByTagName("input");
+	    for (let i = 0; i < up_buttons.length; i++) {
+              if ( (up_buttons[i].disabled == false) && (up_buttons[i].value == "Update") ) {
+		update_list=update_list + ";" + up_buttons[i].id;
+		update_count++;
+              }
+            }
+	    document.getElementById('update_list').value=update_list;
+	    button1=document.getElementById('delete-ALL');
+	    button2=document.getElementById('really-delete-ALL');
+	    if (update_count > 1) {
+		    button1.value="Update All";
+		    button2.value="Yes";
+	    } else {
+		    button1.value="Delete ALL";
+		    button2.value="Yes!";
+	    }
+    };
+  </script>
 <div align="center" style="padding-bottom:5px;">
  Entrants for <select name="EventList" style="width: 240px" onchange="showEntrants(this.value)">
    <?php echo $event_select;?>
@@ -160,14 +216,14 @@
     $safe_special=htmlspecialchars($row['special']);
     $row_id=$row['rowid'];
     echo "<td><input type=\"number\" placeholder=\"Num\" size=\"4\" name=\"EntNum-$row_id\" required min=\"1\" value=\"$safe_num\"";
-    echo " class=\"input_number\" oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_num')\" ></td>\n";
+    echo " class=\"input_number\" oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_num');haveUpdate()\" ></td>\n";
     echo "<td><input type=\"text\" placeholder=\"Entrant Name\" name=\"EntName-$row_id\" class=\"txtField\" required value=\"$safe_name\"";
-    echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_name')\" ></td>\n";
+    echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_name');haveUpdate()\" ></td>\n";
     echo "<td><input type=\"text\" placeholder=\"Entrant Info\" name=\"EntInfo-$row_id\" class=\"txtField\" value=\"$safe_info\"";
-    echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_info')\" ></td>\n";
-    echo "<td><input type=\"text\" placeholder=\"Entrant Special\" name=\"EntSpecial-$row_id\" class=\"txtField\" value=\"$safe_special\"";
-    echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_special')\" ></td>\n";
-    echo "<td> <input id=\"submit-$row_id\" type=\"submit\" name=\"submit\" value=\"Update\" formaction=\"?evt=$evt&id=$row_id\" class=\"button\" disabled> </td>\n";
+    echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_info');haveUpdate()\" ></td>\n";
+    echo "<td><input type=\"text\" placeholder=\"Special\" size=\"10\" name=\"EntSpecial-$row_id\" class=\"txtField\" value=\"$safe_special\"";
+    echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_special');haveUpdate()\" ></td>\n";
+    echo "<td> <input id=\"submit-$row_id\" type=\"submit\" name=\"submit\" value=\"Update\" tag=\"Update\" formaction=\"?evt=$evt&id=$row_id\" class=\"button\" disabled> </td>\n";
     echo "<td> <input id=\"delete-$row_id\" type=\"button\" name=\"delete-$row_id\" value=\"Delete\" onclick=\"document.getElementById('really-delete-$row_id').disabled=false\" class=\"button\"> </td>\n";
     echo "<td> <input id=\"really-delete-$row_id\" type=\"submit\" name=\"really-delete\" value=\"Yes!\" formnovalidate formaction=\"?evt=$evt&id=$row_id\" class=\"button\" disabled> </td>\n";
     echo "</tr>\n";
@@ -190,7 +246,7 @@
    echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_name')\" ></td>\n";
    echo "<td><input type=\"text\" placeholder=\"Entrant Info\" name=\"EntInfo-$row_id\" class=\"txtField\" value=\"$safe_info\"";
    echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_info')\" ></td>\n";
-   echo "<td><input type=\"text\" placeholder=\"Entrant Special\" name=\"EntSpecial-$row_id\" class=\"txtField\" value=\"$safe_special\"";
+   echo "<td><input type=\"text\" placeholder=\"Special\" size=\"10\" name=\"EntSpecial-$row_id\" class=\"txtField\" value=\"$safe_special\"";
    echo " oninput=\"document.getElementById('submit-$row_id').disabled=(this.value == '$safe_special')\" ></td>\n";
    echo "<td> <input id=\"submit-$row_id\" type=\"submit\" name=\"submit\" value=\"Create\" formaction=\"?evt=$evt&id=$row_id\" class=\"button\" disabled> </td>\n";
    echo "</td></tr>\n";
