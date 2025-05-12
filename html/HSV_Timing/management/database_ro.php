5be0aaa7803cf33d579afdfee1c94b5e1ef43a17
David Deaves <HSV_Timing@dd.id.au>
2025-05-12 16:27:31 +1000

    Fix the syntax

--- html/HSV_Timing/management/database_ro.php
+++ html/HSV_Timing/management/database_ro.php
@@ -4,7 +4,8 @@
   $config = yaml_parse_file( "/etc/timing/timing.conf");
   if ((isset($config['database_path'])) &&
       file_exists($config['database_path'])) {
-      $db_file = $config['database_path'])
+      $db_file = $config['database_path'];
+  }
 
   $db = new SQLite3($db_file, SQLITE3_OPEN_READONLY);
   $db->busyTimeout(10000);
