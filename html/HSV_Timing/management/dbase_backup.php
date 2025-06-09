7f72ed60fd0b1176229c0043e969f2fe373c7f35
David Deaves <HSV_Timing@dd.id.au>
2025-06-09 20:37:22 +1000

    Add a link to backup the database

---
+++ html/HSV_Timing/management/dbase_backup.php
@@ -0,0 +1,25 @@
+<?php
+  $db_file='/data/Track_Time/Track_Time.db';
+
+  $config = yaml_parse_file( "/etc/timing/timing.conf");
+  if ((isset($config['database_path'])) &&
+      file_exists($config['database_path'])) {
+      $db_file = $config['database_path'];
+  }
+
+  if(isset($argc) && ($argc>1))
+    parse_str(implode('&',array_slice($argv, 1)), $_GET);
+
+  $basepos = strrpos($db_file, "/", 0) + 1;
+  if (false === $basepos)
+    $basename = $db_file;
+  else
+    $basename = substr($db_file, $basepos);
+
+  // Set PHP headers for CSV output.
+  header('Content-Type: application/vnd.sqlite3');
+  header('Content-Disposition: attachment; filename=' . $basename);
+
+  readfile($db_file);
+
+?>
