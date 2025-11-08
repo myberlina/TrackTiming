8521719630b5317f48a53e9633544f6f0f5e9cb3
David Deaves <HSV_Timing@dd.id.au>
2025-11-08 16:09:24 +1100

    Update TriSeries scores Add a blank line to management.html to make clicking easier

--- etc/timing/TriSeriesScores.php
+++ etc/timing/TriSeriesScores.php
@@ -3,5 +3,5 @@
 
   // $scores=array( array( "SDMA" => 40, "WSCC" => 28, "CMHAC" => 24 ), array( "SDMA" => 42, "WSCC" => 24, "CMHAC" => 26 ) );
   // $scores=array( array( "SDMA" => 40, "WSCC" => 28, "CMHAC" => 24 ), array( "SDMA" => 0, "WSCC" => 0, "CMHAC" => 0 ) );
-  $scores=array( array( "SDMA" => 40, "WSCC" => 28, "CMHAC" => 24 ) );
+  $scores=array( array( "SDMA" => 40, "WSCC" => 28, "CMHAC" => 24 ), array( "SDMA" => 34, "WSCC" => 34, "CMHAC" => 23 ) );
 ?>
